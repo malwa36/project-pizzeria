697c72967486342ec14d839f24b305430a0f95a2
Malwina Leśniewska <malwinalesniewska@gazeta.pl>
2022-05-19 06:50:11 +0200

    add method 'initAccordion'

--- src/js/script.js
+++ src/js/script.js
@@ -53,50 +53,77 @@
   };
 
   class Product{
-  	constructor(id, data){
-		const thisProduct = this;
+    constructor(id, data){
+      const thisProduct = this;
 		
-		thisProduct.id = id;
-		thisProduct.data = data;
+      thisProduct.id = id;
+      thisProduct.data = data;
 		
-		thisProduct.renderInMenu();
-		console.log('new Product:', thisProduct);
-	}
+      thisProduct.renderInMenu();
+      console.log('new Product:', thisProduct);
+      thisProduct.initAccordion();
+    }
 	
-	renderInMenu(){
-		const thisProduct = this;
+    renderInMenu(){
+      const thisProduct = this;
 		
-		/* generate HTML based on template */
-		const generatedHTML = templates.menuProduct(thisProduct.data);
+      /* generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
 		
-		/* create element using utils.createElementFromHTML */
-		thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
 		
-		/* find menu container */
-		const menuContainer = document.querySelector(select.containerOf.menu);
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
 		
-		/* add element to menu */
-		menuContainer.appendChild(thisProduct.element);
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
 		
-	}
+    }
+    initAccordion(){
+      const thisProduct = this;
+		
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+      
+        /* prevent default action for event */
+        event.preventDefault();
+
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+		  
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if(activeProduct != thisProduct.element && activeProduct != null){
+			activeProduct.classList.remove('active');
+		}
+		
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle('active');
+		
+      });
+		
+    }
   }
   
   const app = {
-	initMenu: function (){
-		const thisApp = this;
-		console.log('thisApp.data:', thisApp.data);
-		  
-	  	for(let productData in thisApp.data.products){
-			new Product(productData, thisApp.data.products[productData]);
-		}  
-	},
-	  
-	initData: function(){
-		const thisApp = this;
+    initMenu: function (){
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);  
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }  
+    },
+
+    initData: function(){
+      const thisApp = this;
 		
-		thisApp.data = dataSource;
-	},  
-	  
+      thisApp.data = dataSource;
+    },  
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -104,10 +131,10 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
-	  
-	  thisApp.initData();
-	  thisApp.initMenu();
-      },
+
+      thisApp.initData();
+      thisApp.initMenu();
+    },
   };
   
   app.init();
