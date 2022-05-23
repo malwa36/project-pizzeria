6bb17e898274d5f549a68697c46d36ee09382735
Malwina Leśniewska <malwinalesniewska@gazeta.pl>
2022-05-23 17:07:25 +0200

    clear my code from errors

--- src/js/script.js
+++ src/js/script.js
@@ -84,7 +84,7 @@
 		
     }
 	
-	getElements(){
+    getElements(){
       const thisProduct = this;
 
       thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
@@ -120,55 +120,55 @@
       });	
     }
 	
-	initOrderForm(){
-		const thisProduct = this;
-		console.log('initOrderForm');
+    initOrderForm(){
+      const thisProduct = this;
+      console.log('initOrderForm');
 		
-		thisProduct.form.addEventListener('submit', function(event){
-          event.preventDefault();
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
           thisProduct.processOrder();
         });
+      }
 
-        for(let input of thisProduct.formInputs){
-          input.addEventListener('change', function(){
-          thisProduct.processOrder();
-          });
-        }
-
-        thisProduct.cartButton.addEventListener('click', function(event){
-          event.preventDefault();
-          thisProduct.processOrder();
-        });
-	};
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
 	
-	processOrder(){
-		const thisProduct = this;
-		console.log('processOrder');
+    processOrder(){
+      const thisProduct = this;
+      console.log('processOrder');
 		
-		// covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
-        const formData = utils.serializeFormToObject(thisProduct.form);
-        console.log('formData', formData);
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
 
-        // set price to default price
-        let price = thisProduct.data.price;
+      // set price to default price
+      let price = thisProduct.data.price;
 
-        // for every category (param)...
-        for(let paramId in thisProduct.data.params) {
-          // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
-          const param = thisProduct.data.params[paramId];
-          console.log(paramId, param);
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
 
-          // for every option in this category
-          for(let optionId in param.options) {
-            // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
-            const option = param.options[optionId];
-            console.log(optionId, option);
-          }
-        } 
+        // for every option in this category
+        for(let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          console.log(optionId, option);
+        }
+      } 
 
-        // update calculated price in the HTML
-        thisProduct.priceElem.innerHTML = price;
-	  }
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
+    }
 	
   }
   
